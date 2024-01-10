9cba45e1e88ba95ff1162d4cb680ef2147697038
micahyanoh <ogongoyanojnr@gmail.com>
2024-01-10 23:45:14 +0300

    updates

--- lib/recursion/sum_tail.ex
+++ lib/recursion/sum_tail.ex
@@ -1,7 +1,6 @@
-defmodule Tutorials.Recursion.SumTail do
-  def upto(num,acc \\ 0)
-  def upto(0,acc), do: acc
-  def upto(num, acc) do
-    upto(num - 1, acc + num)
-  end
+defmodule Tutorials.Recursion.Sum do
+  def get_sum(n), do: get_sum(n, 0)
+
+  defp get_sum(0, acc), do: acc
+  defp get_sum(n, acc) when n > 0, do: get_sum(n - 1, n + acc)
 end
