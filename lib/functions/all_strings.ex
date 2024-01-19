5a49b6fc1366ad2e236994c0c12f0b0dc8435756
micahyanoh <ogongoyanojnr@gmail.com>
2024-01-19 23:14:29 +0300

    updates

---
+++ lib/functions/all_strings.ex
@@ -0,0 +1,20 @@
+defmodule Tutorials.Functions.AllStrings do
+    @moduledoc """
+    using the inbuilt functions of the string module
+    to manipulate strings
+    """
+    def first_letter(value) do
+        String.trim(value)
+        |> String.first()
+    end 
+
+    @doc """
+    "|>" -> pipe operator
+    used to pass a variable to a number of functions without 
+    redeclaring it
+    """
+    def initials(value) do
+        first_letter(value)
+        |> String.capitalize()
+    end
+end
